276dd4c7fb9fa0f8f4630c394fc9ce0c6db8afa2
YanHF <yan.1754@students.amikom.ac.id>
2021-06-28 13:31:31 +0700

    add comment in previous slots

--- resources/views/bulletin/slots/previousButton.blade.php
+++ resources/views/bulletin/slots/previousButton.blade.php
@@ -1,3 +1,7 @@
 <a class="btn btn-lg text-center" href="{{ url('bulletin?page=' . Session::get('currentPage')) }}">
     Back previous page
 </a>
+
+{{-- if code like this, this code only can use to back to previous page in pagination --}}
+
+{{-- option to make it reusable, convert it into component and receive url as props --}}
