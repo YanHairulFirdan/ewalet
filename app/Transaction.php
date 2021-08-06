679c1e5af0b4adf832ed6f6a494cf3e6b83e53ba
YanHF <yan.1754@students.amikom.ac.id>
2021-08-06 15:49:12 +0700

    create 50 fake users

--- app/Transaction.php
+++ app/Transaction.php
@@ -4,8 +4,14 @@ namespace App;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 
 class Transaction extends Model
 {
     protected $fillable = ['user_id', 'buyer', 'weight', 'price_per_kilo', 'total_price'];
+
+    public function user()
+    {
+        return $this->belongsTo(App\User::class);
+    }
 }
