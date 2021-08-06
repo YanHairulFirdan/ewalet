30a9b025913c0c752cacd7e709a9cd3bad07209f
YanHF <yan.1754@students.amikom.ac.id>
2021-08-06 16:00:27 +0700

    make login controller for admin

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function showForm()
+    {
+        // return view('admin.login');
+    }
+    public function login(Request $request)
+    {
+    }
+
+    public function logout(Request $request)
+    {
+        // logout
+    }
+}
