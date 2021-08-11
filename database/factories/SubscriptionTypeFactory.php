8cb2a674df82117ebdb138300e4db83cdc6b2500
YanHF <yan.1754@students.amikom.ac.id>
2021-08-11 07:32:34 +0700

    make factories and seeders for payment, subscription and subscriptiontype

---
+++ database/factories/SubscriptionTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\SubscriptionType;
+use Faker\Generator as Faker;
+
+$factory->define(SubscriptionType::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
