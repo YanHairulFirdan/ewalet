17cd472985c25f94024245b803ac54d25937d023
YanHF <yan.1754@students.amikom.ac.id>
2021-08-11 07:36:38 +0700

    fill SubscriptionTypeSeeder

--- database/seeds/SubscriptionTypeSeeder.php
+++ database/seeds/SubscriptionTypeSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\SubscriptionType;
 use Illuminate\Database\Seeder;
 
 class SubscriptionTypeSeeder extends Seeder
@@ -11,6 +12,17 @@ class SubscriptionTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $names = [
+            'Coba Gratis' => 0,
+            'Bulanan'     => 10000,
+            'Tahunan'     => 100000,
+        ];
+
+        foreach ($names as $name => $price) {
+            SubscriptionType::create([
+                'name'  => $name,
+                'price' => $price,
+            ]);
+        }
     }
 }
