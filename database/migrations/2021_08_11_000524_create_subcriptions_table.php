e5e69991757db04308e1e9f1451a04a5d8698c91
YanHF <yan.1754@students.amikom.ac.id>
2021-08-11 07:12:49 +0700

    make subscriptions migration

---
+++ database/migrations/2021_08_11_000524_create_subcriptions_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubcriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcriptions', function (Blueprint $table) {
+            $table->id();
+            $table->bigIncrements('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->bigIncrements('type_id');
+            $table->foreign('type_id')
+                ->references('id')
+                ->on('types')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamp('started_at');
+            $table->timestamp('end_at');
+            $table->timestamps();
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcriptions');
+    }
+}
