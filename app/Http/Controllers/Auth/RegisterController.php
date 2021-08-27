3374423a762fd5388410302ca85939e9f629e150
YanHF <yan.1754@students.amikom.ac.id>
2021-08-27 13:48:41 +0700

    register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,10 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        // dd($data);
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone_number' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -66,7 +67,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'phone_number' => $data['phone_number'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,51 +5,46 @@
 @endsection
 
 @section('content')
-    <div class="col-lg-7">
+    <div class="col-lg-12">
         <div class="card shadow-lg border-0 rounded-lg mt-5">
             <div class="card-header">
                 <h3 class="text-center font-weight-light my-4">Create Account</h3>
             </div>
             <div class="card-body">
-                <form>
-                    <div class="row mb-3">
-                        <div class="col-md-6">
-                            <div class="form-floating mb-3 mb-md-0">
-                                <input class="form-control" id="inputFirstName" type="text"
-                                    placeholder="Enter your first name" />
-                                <label for="inputFirstName">First name</label>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input class="form-control" id="inputLastName" type="text"
-                                    placeholder="Enter your last name" />
-                                <label for="inputLastName">Last name</label>
-                            </div>
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
+                    @method('POST')
+                    <div class="form-group">
+                        <div class="form-floating mb-3 mb-md-0">
+                            <input class="form-control" id="inputName" type="text" name="name"
+                                placeholder="Enter your name" />
+                            <label for="inputName">Name</label>
                         </div>
                     </div>
-                    <div class="form-floating mb-3">
-                        <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                        <label for="inputEmail">Email address</label>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-md-6">
-                            <div class="form-floating mb-3 mb-md-0">
-                                <input class="form-control" id="inputPassword" type="password"
-                                    placeholder="Create a password" />
-                                <label for="inputPassword">Password</label>
-                            </div>
+                    <div class="form-group">
+                        <div class="form-floating mb-3 mb-md-0">
+                            <input class="form-control" id="inputPhoneNumber" type="text" name="phone_number"
+                                placeholder="Enter your phone number" />
+                            <label for="inputPhoneNumber">Phone Number</label>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-floating mb-3 mb-md-0">
-                                <input class="form-control" id="inputPasswordConfirm" type="password"
-                                    placeholder="Confirm password" />
-                                <label for="inputPasswordConfirm">Confirm Password</label>
-                            </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-floating mb-3 mb-md-0">
+                            <input class="form-control" id="inputPassword" type="text" name="password"
+                                placeholder="Enter your password" />
+                            <label for="inputPassword">Password</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-floating mb-3 mb-md-0">
+                            <input class="form-control" id="inputPasswordCOnfirmation" type="text"
+                                name="password_confirmation" placeholder="Confirm your password" />
+                            <label for="inputPasswordCOnfirmation">Password</label>
                         </div>
                     </div>
                     <div class="mt-4 mb-0">
-                        <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">Create Account</a></div>
+                        <div class="d-grid"><button type="submit" class="btn btn-primary btn-block">Create
+                                Account</button></div>
                     </div>
                 </form>
             </div>
