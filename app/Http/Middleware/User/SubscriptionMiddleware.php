df2da53e1e9d759750a6d3cff1eb17f9496b6d31
YanHF <yan.1754@students.amikom.ac.id>
2021-08-31 13:34:40 +0700

    make and apply subscription middleware

---
+++ app/Http/Middleware/User/SubscriptionMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware\User;
+
+use Closure;
+
+class SubscriptionMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user()->subscription->status) {
+            return redirect(route('home'));
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@Index')->name('home');
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'active'])->group(function () {
     Route::post('transactions/{transaction}', 'TransactionController@update');
     Route::resource('transactions', TransactionController::class)->except(['create', 'edit', 'update']);
 });
-// Route::controller('users', 'UserController');
-// Route::get('bulletin', 'BulletinController@index')->name('bulletin.index');
-// Route::post('bulletin', 'BulletinController@store')->name('bulletin.store');
-// Route::get('bulletin/{bulletin}', 'BulletinController@show')->name('bulletin.show');
-// Route::get('bulletin/edit/{bulletin}', 'BulletinController@showEdit')->name('bulletin.show.edit');
-// Route::put('bulletin/update/{bulletin}', 'BulletinController@update')->name('bulletin.update');
-// Route::get('bulletin/delete/{bulletin}', 'BulletinController@showDelete')->name('bulletin.show.delete');
-// Route::delete('bulletin/delete/{bulletin}', 'BulletinController@delete')->name('bulletin.delete');
-// Route::post('password/{bulletin}', 'BulletinController@postPassword')->name('post.password');
 
 Auth::routes();
 
