42362299eaa64019af0b458e3c3fd0921e2d3524
YanHF <yan.1754@students.amikom.ac.id>
2021-08-31 13:45:25 +0700

    add status column for payments table

--- app/Http/Controllers/User/SubscriptionController.php
+++ app/Http/Controllers/User/SubscriptionController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    //
+    public function index()
+    {
+        $types = Type::get();
+
+        return view('user.subscription.index', compact('types'));
+    }
 }
