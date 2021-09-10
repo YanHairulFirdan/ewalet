ceffabd57ce62a3f81a9737ee000f1419bcf1bfc
YanHF <yan.1754@students.amikom.ac.id>
2021-09-10 09:27:17 +0700

    fix import user model

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Payment;
 use App\Transaction;
-use App\User;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
