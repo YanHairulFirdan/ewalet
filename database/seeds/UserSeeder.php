9129ec8787d639e8c7cf7e09cbe0a63d64b2a138
YanHF <yan.1754@students.amikom.ac.id>
2021-09-16 15:57:37 +0700

    make user only

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,40 +17,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(App\User::class, 100)->create()->each(function ($user, Faker $faker) {
-            for ($i = 0; $i < rand(0, 40); $i++) {
-                $weight         = $faker->randomNumber(2);
-                $price_per_kilo = rand(8000000, 10000000);
-
-                Transaction::create([
-                    'user_id'        => $user->id,
-                    'buyer'          => $faker->name,
-                    'weight'         => $weight,
-                    'price_per_kilo' => $price_per_kilo,
-                    'total_price'    => $weight * $price_per_kilo,
-                    'created_at'     => Carbon::today()->subDays(rand(0, 365))
-                ]);
-            }
-            $type = Type::inRandomOrder()->first();
-
-            Payment::create([
-                'user_id'    => $user->id,
-                'amount'     => $type->price,
-                'created_at' => Carbon::today()->subDays(rand(0, 365))
-            ]);
-
-            $subscriptionDays = $type->name == 'Coba Gratis' || $type->name == 'Bulanan' ? 30 : 360;
-
-            $started_at = Carbon::now();
-            $ended_at   = Carbon::now()->addDays($subscriptionDays);
-
-            Subscription::create([
-                'user_id' => $user->id,
-                'type_id' => $type->id,
-                'started_at' => $started_at,
-                'end_at'     => $ended_at,
-                'status'     => true
-            ]);
-        });
+        factory(App\User::class, 100)->create();
     }
 }
