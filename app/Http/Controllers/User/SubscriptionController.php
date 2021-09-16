00b3cfd1e9df9187985dc0964331ce57e253e857
YanHF <yan.1754@students.amikom.ac.id>
2021-09-16 17:36:09 +0700

    delete response returned

--- app/Http/Controllers/User/SubscriptionController.php
+++ app/Http/Controllers/User/SubscriptionController.php
@@ -24,12 +24,6 @@ class SubscriptionController extends Controller
 
     public function subscribe(Request $request)
     {
-        return response()->json(
-            [
-                'user_id' => Auth::id(),
-                'type_id' => $request->type
-            ]
-        );
         $request->validate([
             'type' => 'required|exists:types,id'
         ]);
