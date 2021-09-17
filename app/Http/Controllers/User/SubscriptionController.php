1142904016d6dfadc9850fd9411697191e386508
YanHF <yan.1754@students.amikom.ac.id>
2021-09-17 10:20:51 +0700

    logging response from midtrans

--- app/Http/Controllers/User/SubscriptionController.php
+++ app/Http/Controllers/User/SubscriptionController.php
@@ -73,6 +73,9 @@ class SubscriptionController extends Controller
                 break;
         }
 
+        Log::info("request from midtrans " . $midtransResponse);
+        Log::info("payments id " . $transactionId);
+        Log::info("status " . $transactionStatus);
         Transaction::where('id', $transactionId)->update([
             'status' => $status
         ]);
