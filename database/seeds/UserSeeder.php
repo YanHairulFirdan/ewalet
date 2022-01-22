af94a6beabbbb106bec72c8de09db55e0a06d524
yan_backend <yanhairulfirdan@gmail.com>
2022-01-22 21:09:36 +0700

    refactoring user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,7 +2,9 @@
 
 use App\Models\Payment;
 use App\Models\Subscription;
+use App\Models\Transaction as ModelsTransaction;
 use App\Models\Type;
+use App\Models\User;
 use App\Transaction;
 use Carbon\Carbon;
 use Faker\Factory;
@@ -18,15 +20,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Factory::create();
-
-        $users = factory(App\User::class, 100)->create()->each(function ($user) use ($faker) {
+        $users = factory(User::class, 100)->create()->each(function ($user) use ($faker) {
+            $faker = Factory::create();
             for ($i = 0; $i < rand(0, 40); $i++) {
-                $weight         = $faker->randomNumber(2);
-                $price_per_kilo = rand(8000000, 10000000);
+                $weight           = $faker->randomNumber(2);
+                $price_per_kilo   = rand(8000000, 10000000);
+                $type             = Type::inRandomOrder()->first();
+                $subscriptionDays = $type->name == 'Coba Gratis' || $type->name == 'Bulanan' ? 30 : 360;
+                $started_at       = Carbon::now();
 
-                Transaction::create([
-                    'user_id'        => $user->id,
+                $user->transactions([
                     'buyer'          => $faker->name,
                     'weight'         => $weight,
                     'price_per_kilo' => $price_per_kilo,
@@ -34,24 +37,16 @@ class UserSeeder extends Seeder
                     'created_at'     => Carbon::today()->subDays(rand(0, 365))
                 ]);
             }
-            $type = Type::inRandomOrder()->first();
 
-            Payment::create([
-                'user_id'    => $user->id,
+            $user->payments()->create([
                 'amount'     => $type->price,
                 'created_at' => Carbon::today()->subDays(rand(0, 365))
             ]);
 
-            $subscriptionDays = $type->name == 'Coba Gratis' || $type->name == 'Bulanan' ? 30 : 360;
-
-            $started_at = Carbon::now();
-            $ended_at   = Carbon::now()->addDays($subscriptionDays);
-
-            Subscription::create([
-                'user_id' => $user->id,
+            $user->subscription()->create([
                 'type_id' => $type->id,
                 'started_at' => $started_at,
-                'end_at'     => $ended_at,
+                'end_at'     => $started_at->addDays($subscriptionDays),
                 'status'     => true
             ]);
         });
